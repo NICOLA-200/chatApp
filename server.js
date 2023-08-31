bf5561ac15a80a4b8b6c60d97448c05b4a42d37b
NICOLA-200 <fatepepe66@gmail.com>
2023-08-31 11:31:27 +0200

    server.js complete

--- server.js
+++ server.js
@@ -58,3 +58,22 @@ io.on("connection", (socket) => {
     
         io.to(user.room).emit("message", formatMessage(user.username, msg));
       });
+
+
+      socket.on("disconnect", () => {
+          const user = userLeave(socket.id);
+      
+          if (user) {
+            io.to(user.room).emit(
+              "message",
+              formatMessage(botName, `${user.username} has left the chat`)
+            );
+      
+            // Send users and room info
+            io.to(user.room).emit("roomUsers", {
+              room: user.room,
+              users: getRoomUsers(user.room),
+            });
+          }
+        });
+      });
