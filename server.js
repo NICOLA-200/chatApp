ebff7568101cd64d6719bca840db2eefb54033b0
NICOLA-200 <fatepepe66@gmail.com>
2023-08-31 11:30:42 +0200

    socket configured

--- server.js
+++ server.js
@@ -13,3 +13,35 @@ const {
   userLeave,
   getRoomUsers,
 } = require("./utils/users");
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+const botName = "ChatCord Bot";
+
+
+
+// Run when client connects
+io.on("connection", (socket) => {
+    console.log('connecti')
+
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
