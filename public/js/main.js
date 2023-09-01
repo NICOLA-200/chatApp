eb4dabb767a4c769d6049b695f81ac62ffb1776f
NICOLA-200 <fatepepe66@gmail.com>
2023-09-01 12:31:57 +0200

    function added

--- public/js/main.js
+++ public/js/main.js
@@ -47,3 +47,33 @@ chatForm.addEventListener('submit', (e) => {
      e.target.elements.msg.value = '';
      e.target.elements.msg.focus();
    });
+
+   function outputMessage(message) {
+     const div = document.createElement('div');
+     div.classList.add('message');
+     const p = document.createElement('p');
+     p.classList.add('meta');
+     p.innerText = message.username;
+     p.innerHTML += `<span>${message.time}</span>`;
+     div.appendChild(p);
+     const para = document.createElement('p');
+     para.classList.add('text');
+     para.innerText = message.text;
+     div.appendChild(para);
+     document.querySelector('.chat-messages').appendChild(div);
+   }
+   
+   // Add room name to DOM
+   function outputRoomName(room) {
+     roomName.innerText = room;
+   }
+   
+   // Add users to DOM
+   function outputUsers(users) {
+     userList.innerHTML = '';
+     users.forEach((user) => {
+       const li = document.createElement('li');
+       li.innerText = user.username;
+       userList.appendChild(li);
+     });
+   }
