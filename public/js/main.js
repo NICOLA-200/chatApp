56ee46773bcd6795932264c824d11e884388e4a5
NICOLA-200 <fatepepe66@gmail.com>
2023-09-01 12:32:12 +0200

    done

--- public/js/main.js
+++ public/js/main.js
@@ -77,3 +77,11 @@ chatForm.addEventListener('submit', (e) => {
        userList.appendChild(li);
      });
    }
+
+   document.getElementById('leave-btn').addEventListener('click', () => {
+     const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+     if (leaveRoom) {
+       window.location = '../index.html';
+     } else {
+     }
+   });
