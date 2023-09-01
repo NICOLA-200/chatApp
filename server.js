518fefc58cd5e9dea14a1fa34007f3e6b086453d
NICOLA-200 <fatepepe66@gmail.com>
2023-09-01 12:34:29 +0200

    bug fixed

--- server.js
+++ server.js
@@ -78,3 +78,6 @@ io.on("connection", (socket) => {
         });
       });
       
+      const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
